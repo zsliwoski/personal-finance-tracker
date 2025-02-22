cbd145aea63d5bf578bd67c30c8ade53bae17e89
Zarek <zsliwoski@gmail.com>
2025-02-21 21:33:20 -0700

    #3 Basic homepage

--- src/app/components/NavMenu.tsx
+++ src/app/components/NavMenu.tsx
@@ -1,17 +1,32 @@
 "use client"
 import { signIn, signOut, useSession } from "next-auth/react";
+import Image from 'next/image';
+import { Button } from "@/components/ui/button";
 
 function AuthButton() {
     const { data: session } = useSession();
     if (session) {
-        return (<>{session?.user?.email}<br /><button onClick={() => signOut()}>Sign Out</button></>)
+        return (<>{session?.user?.email}<br /><Button onClick={() => signOut()}>Sign Out</Button></>)
     }
-    return (<><br /><button onClick={() => signIn()}>Sign In</button></>)
+    return (<><br /><Button onClick={() => signIn()}>Sign In</Button></>)
 }
 export default function NavMenu() {
     return (
-        <div>
-            <AuthButton />
-        </div>
+        <header style={{ backgroundColor: '#333', color: '#fff', padding: '10px' }}>
+            <nav style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <h1 style={{ margin: '0' }}>TallyUp</h1>
+
+                </div>
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <AuthButton />
+                    <a href="#contact" style={{ color: '#fff', textDecoration: 'none', marginLeft: '20px' }}>Contact</a>
+                    <a href="https://github.com/zsliwoski/personal-finance-tracker" style={{ color: '#fff', textDecoration: 'none', marginLeft: '20px', display: 'flex', alignItems: 'center' }}>
+                        <Image src="github-mark-white.svg" alt="GitHub" width={20} height={20} style={{ marginRight: '5px' }} />
+                        GitHub
+                    </a>
+                </div>
+            </nav>
+        </header>
     );
 }
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Dashboard() {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <p>Welcome to your personal finance tracker dashboard.</p>
+        </div>
+    );
+}
