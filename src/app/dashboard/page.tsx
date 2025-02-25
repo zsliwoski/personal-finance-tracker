cd73e005576f5726cee4faeb8b07e51e1c93a325
Zarek <zsliwoski@gmail.com>
2025-02-24 18:22:58 -0700

    #7 Adding Chart to dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,15 @@
 "use client"
 import { useSession } from "next-auth/react"
 import NavMenu from "../components/NavMenu"
-
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { TransactionChart } from "../components/TransactionChart";
 
 export default function Dashboard() {
     const { data: session } = useSession()
@@ -10,6 +18,7 @@ export default function Dashboard() {
             <NavMenu />
             <h1>Dashboard</h1>
             <p>Welcome, {session?.user?.email}</p>
+            <TransactionChart />
         </div>
     )
 }
